ecf5c949a25d0af48357c24ac3560cf9a294782d
ShagunThakyal <121313784+ShagunThakyal@users.noreply.github.com>
2024-08-10 11:28:47 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,8 +12,8 @@ import {app} from "./app.js"
 
 connectDB()
 .then(()=>{
-  app.listen(PORT, ()=>{
-  console.log(`Server is running on port: ${PORT}`)
+  app.listen(process.env.PORT, ()=>{
+  console.log(`Server is running on port: ${process.env.PORT}`)
   });
 })
 .catch(
