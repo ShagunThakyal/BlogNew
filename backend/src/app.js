b160253c4e2183d8a5f16b5dca10ef79b2823dbc
ShagunThakyal <121313784+ShagunThakyal@users.noreply.github.com>
2024-08-10 16:57:32 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -35,7 +35,7 @@ import commentRouter from "./routes/comment.routes.js"
 import { verifyJWT } from './middleware/auth.middleware.js';
 
 //route declarations / api creation
-app.use("/user", userRouter) // here /user will act as prefix then whatever is the route of userRouter will be added e.g hhtps://localhost/9000/api/v1/user/regiter etc  
+app.use("/api/v1/user", userRouter) // here /user will act as prefix then whatever is the route of userRouter will be added e.g hhtps://localhost/9000/api/v1/user/regiter etc  
 app.use("/api/v1/blog",verifyJWT, blogRouter) // here /blog will act as prefix then '' '' '' '' '' '' '' '' '' 
 app.use("/api/v1/share", shareRouter)// here /blog will act as prefix 
 app.use("/api/v1/like", likeRouter)
